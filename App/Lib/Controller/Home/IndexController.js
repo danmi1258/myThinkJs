3a8f6b4f5082a9ca27bacf00777b604c0b01f95e
chenxing <xing393939@qq.com>
2015-07-23 15:24:41 +0800

    mysql

--- App/Lib/Controller/Home/IndexController.js
+++ App/Lib/Controller/Home/IndexController.js
@@ -1,13 +1,33 @@
 /**
  * controller
- * @return 
+ * @return
  */
-module.exports = Controller("Home/BaseController", function(){
-  "use strict";
-  return {
-    indexAction: function(){
-      //render View/Home/index_index.html file
-      this.display();
-    }
-  };
+module.exports = Controller("Home/BaseController", function () {
+    "use strict";
+    return {
+        indexAction: function () {
+            //this.end('nnd');
+            this.display();
+        },
+        testAction: function () {
+            var self = this;
+            var userModel = D('test');
+            userModel.add({
+                title: "welefen",
+                text: "xxx",
+                created: "2010-08-16 12:42:15"
+            }).then(function (insertId) {
+                    self.echo(insertId);
+                }).catch(function (err) {
+                    self.echo(err);
+                });
+            userModel.where().find().then(function (data) {
+                self.assign('list', data);
+                self.display();
+            });
+            //this.end(result);
+            //this.assign('list', {title: 'aaaaa'});
+            //this.display();
+        }
+    };
 });
