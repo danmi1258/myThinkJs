621073b5922d2d1d1c460ec46f06399d43d4851a
chenxing <xing393939@qq.com>
2015-07-23 17:04:12 +0800

    websocket

--- App/Lib/Controller/Home/IndexController.js
+++ App/Lib/Controller/Home/IndexController.js
@@ -1,33 +1,55 @@
-/**
- * controller
- * @return
- */
-module.exports = Controller("Home/BaseController", function () {
-    "use strict";
+module.exports = Controller(function () {
+    //websocket列表
+    var wsList = {};
+    setInterval(function () {
+        for (var id in wsList) {
+            wsList[id].send({
+                name: '机器人',
+                text: '每隔10秒我就发一条消息哦'
+            })
+        }
+    }, 10000)
     return {
         indexAction: function () {
-            //this.end('nnd');
             this.display();
         },
-        testAction: function () {
-            var self = this;
-            var userModel = D('test');
-            userModel.add({
-                title: "welefen",
-                text: "xxx",
-                created: "2010-08-16 12:42:15"
-            }).then(function (insertId) {
-                    self.echo(insertId);
-                }).catch(function (err) {
-                    self.echo(err);
+        /**
+         * 建立连接
+         * @return {[type]} [description]
+         */
+        openAction: function () {
+            var websocket = this.http.websocket;
+            var id = websocket.id;
+            for (var wid in wsList) {
+                wsList[wid].send({
+                    name: '系统',
+                    text: 'id_' + id + '进入了聊天室'
                 });
-            userModel.where().find().then(function (data) {
-                self.assign('list', data);
-                self.display();
-            });
-            //this.end(result);
-            //this.assign('list', {title: 'aaaaa'});
-            //this.display();
+            }
+            wsList[id] = websocket;
+            this.http.on("websocket.close", function () {
+                console.log('close')
+                delete wsList[id];
+                for (var wid in wsList) {
+                    wsList[wid].send({
+                        name: 'id_' + id,
+                        text: 'goodbye~~'
+                    });
+                }
+            })
+        },
+        /**
+         * 获取到消息
+         * @return {[type]} [description]
+         */
+        messageAction: function () {
+            var data = this.get();
+            data.name = 'id_' + this.http.websocket.id;
+            data.wslength = Object.keys(wsList).length;
+            //有消息后向所有人广播
+            for (var id in wsList) {
+                wsList[id].send(data);
+            }
         }
-    };
-});
+    }
+})
