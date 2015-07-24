fa1ee11173b6914c2ccb1ed3cbbf8a509fda3971
chenxing <xing393939@qq.com>
2015-07-24 12:07:58 +0800

    爬虫

--- App/Lib/Controller/Home/IndexController.js
+++ App/Lib/Controller/Home/IndexController.js
@@ -1,13 +1,51 @@
-/**
- * controller
- * @return
- */
-module.exports = Controller("Home/BaseController", function(){
-  "use strict";
-  return {
-    indexAction: function(){
-      //render View/Home/index_index.html file
-      this.display();
-    }
-  };
+var fs = require('fs');
+var path = require('path');
+
+module.exports = Controller("Home/BaseController", function () {
+    "use strict";
+    return {
+        indexAction: function () {
+            this.assign('wsport', C('port'));
+            var configsName = filelist(VIEW_PATH + '/config');
+            this.assign('list', configsName);
+            this.display();
+        }
+    };
 });
+
+/// @dirpath:文件夹路径
+/// @sortprop:排序属性，默认mtime
+var filelist = function (dirpath, sortprop) {
+    var validateProp = ['size', 'atime', 'mtime', 'ctime', 'birthtime'];
+
+    sortprop = sortprop || 'mtime';
+    if (validateProp.indexOf(sortprop) === -1) {
+        throw ('无效的排序属性')
+        return;
+    }
+
+    var files;
+    var filesinfo = [];
+    var result = [];
+    try {
+        files = fs.readdirSync(dirpath);
+        files.forEach(function (element) {
+            var stats = fs.statSync(path.join(dirpath, element));
+            var info = { filename: element };
+            info[sortprop] = stats[sortprop];
+            filesinfo.push(info);
+        });
+
+        filesinfo.sort(function (left, right) {
+            return left[sortprop] > right[sortprop] ? -1 : 1;
+        });
+
+        filesinfo.forEach(function (element) {
+            result.push(element.filename);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+
+    return result;
+}
