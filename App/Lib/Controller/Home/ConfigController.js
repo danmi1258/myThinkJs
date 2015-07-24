dca59f158a64694762b9767709f2580d76684c6f
chenxing <xing393939@qq.com>
2015-07-24 15:17:19 +0800

    爬虫

--- App/Lib/Controller/Home/ConfigController.js
+++ App/Lib/Controller/Home/ConfigController.js
@@ -2,13 +2,45 @@ module.exports = Controller("Home/BaseController", function () {
     "use strict";
     return {
         addAction: function () {
-            this.end('add');
+            var self = this;
+            var myPost = this.post();
+            var selector = [];
+            JSON.parse(myPost.levels).forEach(function (element, index) {
+                selector.push({
+                    $: element.selector,
+                    attr: element.attr
+                });
+            });
+            myPost.selector = JSON.stringify(selector);
+            myPost.isPagination = !!myPost.page ? 1 : 0;
+            myPost.mode = 'web';
+            delete myPost.page;
+            delete myPost.levels;
+
+            D('config').thenAdd(myPost, {configName: myPost.configName}).then(function (id) {
+                var result = {
+                    status: true,
+                    info: "保存成功",
+                    error: null
+                };
+                return self.json(result);
+            });
         },
         deleteAction: function () {
             this.end('add');
         },
         editAction: function () {
-            this.end('sss');
+            var self = this;
+            var configName = this.get('name');
+            D('config').where({configName: configName}).find().then(function (data) {
+                //data.selector = JSON.parse(data.selector);
+                var result = {
+                    status: true,
+                    data: data,
+                    configName: data.configName
+                };
+                return self.json(result);
+            });
         }
     };
 });
--- App/Lib/Controller/Home/IndexController.js
+++ App/Lib/Controller/Home/IndexController.js
@@ -1,51 +1,18 @@
-var fs = require('fs');
-var path = require('path');
-
 module.exports = Controller("Home/BaseController", function () {
     "use strict";
     return {
         indexAction: function () {
+            var self = this;
             this.assign('wsport', C('port'));
-            var configsName = filelist(VIEW_PATH + '/config');
-            this.assign('list', configsName);
-            this.display();
+            D('config').field('configName').select().then(function(data){
+                var list = [];
+                data.forEach(function(row) {
+                   list.push(row.configName);
+                });
+                self.assign('wsport', C('port'));
+                self.assign('list', list);
+                return self.display();
+            });
         }
     };
 });
-
-/// @dirpath:文件夹路径
-/// @sortprop:排序属性，默认mtime
-var filelist = function (dirpath, sortprop) {
-    var validateProp = ['size', 'atime', 'mtime', 'ctime', 'birthtime'];
-
-    sortprop = sortprop || 'mtime';
-    if (validateProp.indexOf(sortprop) === -1) {
-        throw ('无效的排序属性')
-        return;
-    }
-
-    var files;
-    var filesinfo = [];
-    var result = [];
-    try {
-        files = fs.readdirSync(dirpath);
-        files.forEach(function (element) {
-            var stats = fs.statSync(path.join(dirpath, element));
-            var info = { filename: element };
-            info[sortprop] = stats[sortprop];
-            filesinfo.push(info);
-        });
-
-        filesinfo.sort(function (left, right) {
-            return left[sortprop] > right[sortprop] ? -1 : 1;
-        });
-
-        filesinfo.forEach(function (element) {
-            result.push(element.filename);
-        });
-    } catch (error) {
-        console.log(error);
-    }
-
-    return result;
-}
